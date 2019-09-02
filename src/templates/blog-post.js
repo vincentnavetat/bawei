7671d63f9af8d1d26abecb3baebdfd4d98bd31a1
Vincent Navetat <vincent.navetat@carwow.co.uk>
2019-09-02 17:16:47 +0100

    Fix post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,10 +10,11 @@ class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
+    const headerLinks = this.props.data.site.siteMetadata.menuLinks
     const { previous, next } = this.props.pageContext
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location} title={siteTitle} headerLinks={headerLinks}>
         <SEO
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
@@ -88,6 +89,10 @@ export const pageQuery = graphql`
       siteMetadata {
         title
         author
+        menuLinks {
+          name
+          link
+        }
       }
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
